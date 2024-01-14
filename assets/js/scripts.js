6e407aff42c3f4a1de84666d381243a90ea00092
Steven Fountain <stevenfountain@Stevens-MacBook-Pro.local>
2024-01-14 12:12:32 -0800

    update inventory links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,30 @@
 function onSubmit(token) {
-  document.getElementById("contact").submit();
+  document.getElementById("submit-contact-button").submit();
 }
 
-$(document).ready(function () {
-  $(".navbar-nav li a").click(function (event) {
-    $(".offcanvas").offcanvas('hide');
-  });
+$(document).ready(() => {
+  $(".navbar-nav li a").click(hideOffcanvasAfterDelay);
+});
+
+function hideOffcanvasAfterDelay() {
+  setTimeout(() => $(".offcanvas").offcanvas('hide'), 750);
+}
+
+function generateUrlAndRedirect(trailerId) {
+  // Get the value from the input element
+  let inputValue = document.getElementById(trailerId).innerText;
+
+  // Construct the URL with the parameter
+  let url = 'index.html?trailer=' + encodeURIComponent(inputValue) + '#contact';
+
+  // Redirect to the generated URL
+  window.location.href = url;
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  let urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('trailer')) {
+    document.getElementById('request-text').value = urlParams.get('trailer');
+  }
 });
