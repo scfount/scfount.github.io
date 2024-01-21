27e64063fa4a5d710badd287484d237ae7369dcb
Steven Fountain <stevenfountain@Stevens-MacBook-Pro.local>
2024-01-21 15:58:05 -0800

    add contact form functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,3 @@
-function onSubmit(token) {
-  document.getElementById("submit-contact-button").submit();
-}
-
 $(document).ready(() => {
   $(".navbar-nav li a").click(hideOffcanvasAfterDelay);
 });
@@ -22,9 +18,55 @@ function generateUrlAndRedirect(trailerId) {
 
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   let urlParams = new URLSearchParams(window.location.search);
   if (urlParams.has('trailer')) {
     document.getElementById('request-text').value = urlParams.get('trailer');
   }
+});
+
+const form = document.getElementById("form");
+const contactForm = document.getElementById("contact-form");
+const submitButton = document.getElementById("submit-button");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  let object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  let json = JSON.stringify(object);
+  submitButton.innerHTML = "<loading-component />";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        form.innerHTML = "<success-message />";
+        result.style.display = "none";
+        window.loa
+      } else {
+        console.log(response);
+        form.innerHTML = "<failure-message />";
+        result.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      form.innerHTML = "<failure-message />";
+    })
+    .then(function () {
+      window.location.href = "#contact";
+      setTimeout(() => {
+        // window.location.reload();
+      }, 5000);
+    });
 });
